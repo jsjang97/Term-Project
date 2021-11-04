219513c0b435fcb35f26e62578928146662f7990
Jang <jjang3@babson.edu>
2021-11-03 23:59:46 -0400

    Project V1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// let toggle = document.querySelector(".nav .toggle-collapse");
+// let collapse = document.querySelectorAll(".nav .collapse");
+
+// toggle.addEventListener("click", function click () {
+//   console.log(collapse);
+//   collapse.forEach((col) => col.classList.toggle("collapse-t"));
+// });
+
+// new Masonry("#post.grid", {
+//   itemSelector: ".grid-item",
+//   gutter: 20,
+// });
